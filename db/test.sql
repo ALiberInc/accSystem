0a9351c4492399ab7f0ee583c591b0f0ef6ef882
钟小强 <jack80342@outlook.com>
2017-08-02 04:38:56 +0900

    generatorConfig.xmlを修正

---
+++ db/test.sql
@@ -0,0 +1,5 @@
+create table "public".test (
+  testid bigint not null
+  , name character varying(256)
+  , primary key (testid)
+);
---
+++ db/user.sql
@@ -0,0 +1,5 @@
+create table "public"."user" (
+  id bigint not null
+  , mail character varying(256)
+  , primary key (id)
+);
