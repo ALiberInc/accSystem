31e013922b903a7e271e73296c39a8bede2d0e62
Jack Zhong <jack80342@outlook.com>
2017-08-02 20:59:49 +0900

    Formを使って画面表示

--- src/main/java/jp/co/aliber/accsystem/controller/AdminController.java
+++ src/main/java/jp/co/aliber/accsystem/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.aliber.accsystem.entity.auto.User;
+import jp.co.aliber.accsystem.form.UserInfoFrom;
 import jp.co.aliber.accsystem.service.AdminService;
 
 @Controller
@@ -36,5 +37,14 @@ public class AdminController {
 		model.addAttribute("email", firstUser.getMail());
 		return "useMybatis";
 	}
-
+	
+	@RequestMapping(value = "/formtest", method = RequestMethod.GET)
+	public String formTest(UserInfoFrom userForm) {
+		User firstUser = adminService.getFirstUser();
+		userForm.setId(firstUser.getId());
+		userForm.setName("Jack");
+		userForm.setEmail(firstUser.getMail());
+		return "useForm";
+	}
+	
 }
