dc03031915c90a1ea544e1456f4d06ecb49a4cb6
YuKouki <yu_honghui@aliber.co.jp>
2017-10-13 14:02:38 +0900

    会社基本情報画面エラーメッセージ追加

--- src/main/java/jp/co/aliber/accsystem/ImmutableValues.java
+++ src/main/java/jp/co/aliber/accsystem/ImmutableValues.java
@@ -11,4 +11,26 @@ public class ImmutableValues {
 	public static final int EXTRA_DEC_PER_PERSON = 1610;
 
 	public static final String underline = "_";
+	/**
+	 * 会社基本情報画面
+	 */
+	//被保険者負担率
+	public static final String EMPLOY_INSURANCE_RATE="3";
+	//端数処理:50捨51入
+	public static final String ROUNDING_HALF_UP = "4";
+	//端数処理:切り捨て
+	public static final String ROUNDING_UP = "1";
+	//端数処理:切り上げ
+	public static final String ROUNDING_DOWN = "0";
+	//保険料率（介護保険該当なし）
+	public static final String HEALTH_INSUTANCE_RATE = "9.91";
+	//保険料率（介護保険該当者）
+	public static final String  HEALTH_INSURANCE_RATE_2="11.56";
+	//保険料率
+	public static final String WELFARE_INSURANCE_RATE = "18.182";
+	//基金免除保険料率
+	public static final String WELFARE_EXEMPTION_RATE = "0.00";
+	//基金独自給付加算率(従業員負担分)
+	public static final String WELFARE_ADDITION_RATE = "0.00";
+	
 }
