3df28ad266debf9e0f86513c53944236b666f5e8
钟小强 <jack80342@outlook.com>
2017-10-16 11:40:53 +0900

    健康保険標準報酬マスタのコメントを修正

--- db/m_health_insurance_standard_payment.sql
+++ db/m_health_insurance_standard_payment.sql
@@ -6,6 +6,6 @@ create table "public".m_health_insurance_standard_payment (
 );
 
 comment on table m_health_insurance_standard_payment is '健康保険標準報酬マスタ';
-comment on column m_health_insurance_standard_payment.income_tax_id  is '標準報酬下限';
-comment on column m_health_insurance_standard_payment.head_household_name is '標準報酬上限';
-comment on column m_health_insurance_standard_payment.dependency_count is '標準月額 ';
+comment on column m_health_insurance_standard_payment.payment_start  is '標準報酬下限';
+comment on column m_health_insurance_standard_payment.payment_end is '標準報酬上限';
+comment on column m_health_insurance_standard_payment.standard_payment is '標準月額 ';
