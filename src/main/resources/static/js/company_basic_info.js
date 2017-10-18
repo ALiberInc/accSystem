88eaa152154e72a73c4e81ec3f6179aab244fd6e
YuKouki <yu_honghui@aliber.co.jp>
2017-10-18 15:06:37 +0900

    会社基本情報設定画面：末日以外の場合のみ、日数を活性にする

---
+++ src/main/resources/static/js/company_basic_info.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('#deadlineAdjustDays').attr("readonly", "readonly");
+	$('#paymentAdjustDays').attr("readonly", "readonly");
+})
+$('input[type=radio][name=deadlineDay]').change(function() {
+	if (this.value == '0') {
+		$('#deadlineAdjustDays').attr("readonly", "readonly");
+		$("#deadlineAdjustDays").val("");
+	} else {
+		$('#deadlineAdjustDays').removeAttr("readonly");
+	}
+});
+$('input[type=radio][name=paymentDay]').change(function() {
+	if (this.value == '0') {
+		$('#paymentAdjustDays').attr("readonly", "readonly");
+		$("#paymentAdjustDays").val("");
+	} else {
+		$('#paymentAdjustDays').removeAttr("readonly");
+	}
+});
