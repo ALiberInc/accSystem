7777e15d4ebe4b1b6320f56aad473efc4d4e7480
钟小强 <jack80342@outlook.com>
2017-10-25 13:58:57 +0900

    filterを削除

--- src/main/java/jp/co/aliber/accsystem/Application.java
+++ src/main/java/jp/co/aliber/accsystem/Application.java
@@ -3,12 +3,9 @@ package jp.co.aliber.accsystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 //MyBatis
 @MapperScan("jp.co.aliber.accsystem.mapper")
-// filter
-@ServletComponentScan(basePackages = "jp.co.aliber.accsystem.filter")
 @SpringBootApplication
 public class Application {
 
